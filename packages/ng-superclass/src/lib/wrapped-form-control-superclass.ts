9f545fc39342e5d4b3049a963567f3676fc631d3
Dmitry A. Efimenko <dmitrief@gmail.com>
2022-12-29 15:37:24 -0700

    Feat/super (#4)

--- packages/ng-superclass/src/lib/wrapped-form-control-superclass.ts
+++ packages/ng-superclass/src/lib/wrapped-form-control-superclass.ts
@@ -1,11 +1,15 @@
+import { Directive } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable, startWith } from 'rxjs';
+import { Observable } from 'rxjs';
+import { startWith } from 'rxjs/operators';
 
 import { WrappedControlSuperclass } from './wrapped-control-superclass';
 
 /**
  * This is a convenience class to make it easier to extend {@linkcode WrappedControlSuperclass} when you specifically want a `FormControl` (as opposed to a `FormArray` or `FormGroup`).
  */
+@Directive()
+// eslint-disable-next-line @angular-eslint/directive-class-suffix
 export abstract class WrappedFormControlSuperclass<
   OuterType,
   InnerType = OuterType
