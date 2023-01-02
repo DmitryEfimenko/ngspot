c61b415993bf7825782c274edd297e23f8f27cbd
Dmitry <dmitrief@gmail.com>
2023-01-02 16:41:56 -0700

    feat(demo): 🔥 add home page

--- apps/demo/src/app/app.routes.ts
+++ apps/demo/src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: '@ngspot/*',
+  },
   {
     path: 'expandable-input',
     loadChildren: () => import('@ngspot/expandable-input-demo'),
