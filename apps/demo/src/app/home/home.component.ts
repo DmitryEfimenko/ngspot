4d2556f8152c9b7c8ba110617b99f6f7621e4d64
Dmitry <dmitrief@gmail.com>
2023-01-02 16:41:56 -0700

    feat(demo): 🔥 add home page

---
+++ apps/demo/src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'ngs-home',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeComponent {}
