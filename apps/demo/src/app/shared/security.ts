e8ee6df7653efea1a49a6471496733b60b9ec231
Nicolas Naso <nnaso@eiwa.ag>
2025-09-12 08:12:43 -0300

    refactor: remove unused functions

--- apps/demo/src/app/shared/security.ts
+++ apps/demo/src/app/shared/security.ts
@@ -1,13 +1,3 @@
-export function fromInnerHTML(el: Element): string {
-  // DEMO: using raw innerHTML. In production, ensure proper sanitization.
-  return el.innerHTML;
-}
-
-export function fromOuterHTML(el: Element): string {
-  // DEMO: using raw outerHTML. In production, ensure proper sanitization.
-  return el.outerHTML;
-}
-
 export function svg(constantSvg: TemplateStringsArray): string {
   // DEMO: constant SVG string. In production, ensure proper sanitization.
   return constantSvg[0];
